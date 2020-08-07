9be964fd876cc2cefd2924ac7f075e5ed87658f5
Juan Manuel Bonoso Gordillo <Juan.Bonoso@innroad.com>
2020-08-07 16:23:27 -0500

    my delete method

--- Trees/MyBinarySearchTree.js
+++ Trees/MyBinarySearchTree.js
@@ -52,7 +52,47 @@ class BinarySearchTree {
         }
         return false;
     }
-    // remove
+    remove(value) {
+        if (!this.root.value) {
+            return false;
+        }
+        let currentNode = this.root;
+        let previousNode = {};
+        while (currentNode) {
+            if (value < currentNode.value) {
+                previousNode.direction = 'left';
+                previousNode.node = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                previousNode.direction = 'right';
+                previousNode.node = currentNode;
+                currentNode = currentNode.right;
+            } else if (value === currentNode.value) {
+                // has no childs
+                if (!currentNode.right && !currentNode.left) {
+                    currentNode = null;
+                    previousNode.node[previousNode.direction] = null;
+                }
+                // has children
+                else if (currentNode.right && currentNode.left) {
+                    const nextRightElement = currentNode.right;
+                    // has granchildren
+                    if (nextRightElement.left) {
+                        previousNode.node[previousNode.direction] = nextRightElement.left;
+                        nextRightElement.left.left = currentNode.left;
+                        nextRightElement.left.right = nextRightElement.right;
+                        currentNode = null;
+                        // has only children
+                    } else {
+                        previousNode.node[previousNode.direction] = nextRightElement;
+                        if (currentNode.left) nextRightElement.left = currentNode.left;
+                        currentNode = null;
+                    }
+                }
+            }
+        }
+    }
+
     traverse(node) {
         const tree = { value: node.value };
         tree.left = node.left === null ? null : this.traverse(node.left);
@@ -72,9 +112,15 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+tree.insert(150);
+tree.insert(180);
+
 tree.print();
 //     9
 //  4     20
 //1  6  15  170
+//        150   180
 
+console.log(tree.lookup(9));
+tree.remove(20);
 console.log(tree.lookup(9));
